f8d5d7a3ce17ed7dde3620048c4585704533ef54
RicaMae001 <jurelminecraftgamer20@gmail.com>
2025-07-06 16:00:49 +0800

    updateDB

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = '192.168.101.81';
 $user = 'remote_root';
 $pass = ''; // basta sa password ni db
 $dbname = 'flower_shop';
-
+$port = 3307; // Port number for MySQL, default is usually 3306
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
