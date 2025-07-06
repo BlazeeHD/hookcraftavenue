11c77a6519ff5027b7b45d36e969cd97243cec28
BlazeeHD <saranganijurelmonterde20@gmail.com>
2025-07-06 15:41:27 +0800

    error

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = '192.168.101.81';
 $port = 3307;
 $user = 'remote_root';
 $pass = ''; // or your MySQL password if set
-$dbname = 'flower_shop';
+
 
 
 $conn = new mysqli($host, $user, $pass, $dbname,$port);
